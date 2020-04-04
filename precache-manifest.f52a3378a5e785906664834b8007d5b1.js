ddb0067cc5051a6130796bc388d62a9294be9081
nicolasariza <nicolas_ariza@outlook.com>
2020-04-04 12:34:32 -0500

    Updates

---
+++ precache-manifest.f52a3378a5e785906664834b8007d5b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31601ca8212e0489bb936bb8d8ab2b8e",
+    "url": "/noticias/index.html"
+  },
+  {
+    "revision": "00d804452c955a416b88",
+    "url": "/noticias/static/css/main.64b1d716.chunk.css"
+  },
+  {
+    "revision": "53b68bb7d2d1d8307c28",
+    "url": "/noticias/static/js/2.f36752ae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/noticias/static/js/2.f36752ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00d804452c955a416b88",
+    "url": "/noticias/static/js/main.3500c48c.chunk.js"
+  },
+  {
+    "revision": "e0d146e82e360778d1ab",
+    "url": "/noticias/static/js/runtime-main.83913b59.js"
+  }
+]);
